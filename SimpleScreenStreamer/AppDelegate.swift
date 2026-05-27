543f4254bf723ab0e49cacef56e29f210f13bcb0
pacpac217 <pacpac217@users.noreply.github.com>
2026-05-27 11:49:28 +0700

    Convert app lifecycle from SceneDelegate to standard, bulletproof AppDelegate window initialization to fix black screen issue under TrollStore

--- SimpleScreenStreamer/AppDelegate.swift
+++ SimpleScreenStreamer/AppDelegate.swift
@@ -6,15 +6,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = ViewController()
+        window.rootViewController = rootViewController
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
